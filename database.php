3233949b0c04e0ba483b5ae366c0401c1716969b
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 18:30:26 -0400

    April 30, 2022 6:30 PM

--- database.php
+++ database.php
@@ -20,4 +20,30 @@
             $connection = mysqli_connect($server, $username, $password, $database);
         }
     }
+
+    function database_verifyUser($username, $password) {
+        // Use the global connection
+        global $connection;
+
+        // Create a default value
+        $status = false;
+
+        if($connection != null) {
+            // Use WHERE expressions to look for username
+            $results = mysqli_query($connection, "SELECT password FROM users WHERE username = '{$username}';");
+            
+            // mysqli_fetch_assoc() returns either null or row data
+            $row = mysqli_fetch_assoc($results);
+            
+            // If $row is not null, it found row data.
+            if($row != null) {
+                // Verify password against saved hash
+                if(password_verify($password, $row["password"])) {
+                    $status = true;
+                }
+            }
+        }
+
+        return $status;
+    }
 ?>
