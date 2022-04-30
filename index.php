22990a6cb6d1b48fe7df1cc7cec627e6a68d487d
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 19:10:09 -0400

    April 30, 2022 7:10 PM

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-    <head></head>
+    <head>  
+        <link rel="stylesheet" href="styles.css">
+    </head>
 
 <?php
 include("security.php");
 
 if(secure_loggedIn()) {
-echo('<a href="delete.php">Delete Account</a><br>');
-echo('<br><a href="logout.php">Log Out</a>');
+echo('<a href="delete.php" class="anchor">Delete Account</a><br>');
+echo('<br><a href="logout.php"  class="anchor">Log Out</a>');
 }
 
 else {
     echo('<h1>Welcome to your to-do app!</h1><br>');
     echo('<p>Log in below or create a new account to get started.</p>');
     echo('<a href="newaccount.php" class="anchor">Create Account</a><br>');
-    echo('<a href="login.php">Login</a><br>');    
+    echo('<a href="login.php" class="anchor">Login</a><br>');    
 }
 ?>
 
