09a325efc015f1f78eecf64cb48f01f0eb7671ec
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 18:29:43 -0400

    April 30, 2022 6:29 PM

--- database.php
+++ database.php
@@ -1,4 +1,23 @@
 <?php
     // Global connection
     $connection = null;
+
+    function database_connect() {
+        // Use the global connection
+        global $connection;
+
+        // Server
+        $server = "localhost";
+        // Username
+        $username = "root";
+        // If using XAMPP, 
+        //  the password is an empty string.
+        $password = "root";
+        // Database
+        $database = "lab";
+
+        if($connection == null) {
+            $connection = mysqli_connect($server, $username, $password, $database);
+        }
+    }
 ?>
