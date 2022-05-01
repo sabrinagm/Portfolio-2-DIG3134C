ffe6917d9de0482d76acc966fd7824d71a4e6093
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 20:58:42 -0400

    April 30, 2022 8:58 PM

--- todo.php
+++ todo.php
@@ -1,3 +1,23 @@
+<?php 
+    // initialize errors variable
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "todo");
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: index.php');
+		}
+	}
+?>
+	
 <!DOCTYPE html>
     <head>  
         <link rel="stylesheet" href="styles.css">
