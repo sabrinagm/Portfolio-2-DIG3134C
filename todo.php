e605599df0e2aaf29e3574d0224a8d472e7adf20
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 21:03:31 -0400

    April 30, 2022 9:03 PM

--- todo.php
+++ todo.php
@@ -9,15 +9,11 @@ include("database.php");
 
 	// insert a quote if submit button is clicked
 	if (isset($_POST['submit'])) {
-		if (empty($_POST['task'])) {
-			$errors = "You must fill in the task";
-		}else{
 			$task = $_POST['task'];
 			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
 			mysqli_query($db, $sql);
 			header('location: index.php');
 		}
-	}
 ?>
 
 <!DOCTYPE html>
