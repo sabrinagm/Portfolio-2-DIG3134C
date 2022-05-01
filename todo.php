948bc59af9c30feb6f547094e9170eacf146d893
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 20:18:43 -0400

    April 30, 2022 8:18 PM

--- todo.php
+++ todo.php
@@ -4,5 +4,11 @@
     </head>
 <body>
 <h1 class="todoheader" >Today's a great day to check some things off your list!</h1>
+
+<form action="todo.php" method="POST">
+
+    <input>
+    
+</form>
 </body>
 </html>
