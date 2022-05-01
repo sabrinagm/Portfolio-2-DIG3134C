f484621845d693ef809c6c2ae9e50fb5af460bb2
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 20:43:37 -0400

    April 30, 2022 8:43 PM

--- todo.sql
+++ todo.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE `todolist`;
-USE `todolist`;
+CREATE DATABASE `todo`;
+USE `todo`;
 
-CREATE TABLE `todolist` (
-  `id` int(10) AUTO_INCREMENT,
+CREATE TABLE `todo` (
+  `id` int(11) AUTO_INCREMENT,
   `task` varchar(255),
   PRIMARY KEY(`id`)
 );
 
-INSERT INTO `todolist` (`id`, `task`) VALUES
-(1, 'Finish assignment');
+INSERT INTO `todo` (`id`, `task`) VALUES
+(1, 'Walk dog');
