a72c88d099530835d7a8fd224216e6f1dda86feb
sabrinagm <97575342+sabrinagm@users.noreply.github.com>
2022-04-30 20:15:53 -0400

    April 30, 2022 8:15 PM

--- todo.php
+++ todo.php
@@ -3,6 +3,6 @@
         <link rel="stylesheet" href="styles.css">
     </head>
 <body>
-<h1>Today's a great day to check some things off your list!</h1>
+<h1 class="todoheader" >Today's a great day to check some things off your list!</h1>
 </body>
 </html>
--- todo.sql
+++ todo.sql
@@ -3,10 +3,9 @@ USE `todolist`;
 
 CREATE TABLE `todolist` (
   `id` int(10) AUTO_INCREMENT,
-  `username` varchar(255),
-  `password` varchar(255),
+  `task` varchar(255),
   PRIMARY KEY(`id`)
 );
 
-INSERT INTO `accounts` (`id`, `username`, `password`) VALUES
-(1, 'sabrina', '$2y$10$jhSIk2N5BnkEEzgEBWQDw.AUQIEcrH8V0AcNLfW2nkjTAH2WgAAlW');
+INSERT INTO `todolist` (`id`, `task`) VALUES
+(1, 'Finish assignment');
